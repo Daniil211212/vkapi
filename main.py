59090dd0792d6b76ad7483fa4b3ed74a24e61da4
Daniil211212 <62184970+Daniil211212@users.noreply.github.com>
2021-07-31 12:01:40 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+main_token = '66789f57316dd90cd3714d0b4de68d3235214ca67637b69d7891d461bb66b382e7f645aae616edd2fdd5c'
+
+vk_session = vk_api.VkApi(token=main_token)
+longpoll = VkLongPoll(vk_session)
+
+def sender(id, text):
+    vk_session.method('messages_send', {'chat_id': id, 'message' : text, 'random_id' : 0})
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW:
+        if event.from_chat:
+            msg = event.text.lower()
+            id = event.chat_id
+            if msg == "test":
+                sender(id, "text")
