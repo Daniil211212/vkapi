0538c801946e91a9407b4a284f231c39a52356e4
Daniil211212 <62184970+Daniil211212@users.noreply.github.com>
2021-07-31 12:19:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ vk_session = vk_api.VkApi(token=main_token)
 longpoll = VkLongPoll(vk_session)
 
 def sender(id, text):
-    vk_session.method('messages.send', {'chat_id': id, 'message' : text, 'random_id' : 0})
+    vk_session.method('messages_send', {'chat_id': id, 'message' : text, 'random_id' : 0})
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW:
         if event.from_chat:
             msg = event.text.lower()
             id = event.chat_id
-            if msg == "test":
-                sender(id, "text")
+            if msg == "#pokex5":
+                sender(id, "@all, @all, @all, @all, @all. Poke Машина запущена")
